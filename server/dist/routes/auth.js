eab9aebc834d3b0b24cb905707125d65033c4995
Tsvetislav Todorov <tsvetislav.todorov@ad.mentormate.bg>
2022-06-01 15:51:33 +0300

    Updated .gitignore

--- server/dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRoutes = void 0;
-var express_1 = require("express");
-exports.authRoutes = (0, express_1.Router)();
-exports.authRoutes.get('/', function (req, res) {
-    res.send('hi');
-});
--- server/dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-var express_1 = require("express");
-var auth_1 = require("./auth");
-exports.routes = (0, express_1.Router)();
-exports.routes.use('/auth', auth_1.authRoutes);
