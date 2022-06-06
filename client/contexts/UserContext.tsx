bcdd80fba3177d713320f83b8e62246d2473e27a
Tsvetislav Todorov <tsvetislav.todorov@ad.mentormate.bg>
2022-06-06 11:54:11 +0300

    Front-end: User context optimization

--- client/contexts/UserContext.tsx
+++ client/contexts/UserContext.tsx
@@ -32,22 +32,35 @@ type User = {
 
 export const UserProvider = ({ children }: Props) => {
     const token = Cookies.get('CA_J7');
-    const { data, error } = useSWR(
-        ['http://localhost:8089/auth/me', token],
-        getFetcher
-    );
-    const [user, setUser] = useState(initialValue);
-    useEffect(() => {
-        if (data) {
-            setUser(() => ({ username: data.user.username, id: data.user.id }));
-        }
-    }, [data]);
+    if (token) {
+        const { data, error } = useSWR(
+            ['http://localhost:8089/auth/me', token],
+            getFetcher
+        );
+        const [user, setUser] = useState(initialValue);
+        useEffect(() => {
+            if (data) {
+                setUser(() => ({
+                    username: data.user.username,
+                    id: data.user.id,
+                }));
+            }
+        }, [data]);
 
-    return (
-        <UserContext.Provider value={{ username: user.username, id: user.id }}>
-            {children}
-        </UserContext.Provider>
-    );
+        return (
+            <UserContext.Provider
+                value={{ username: user.username, id: user.id }}
+            >
+                {children}
+            </UserContext.Provider>
+        );
+    } else {
+        return (
+            <UserContext.Provider value={{ username: '', id: '' }}>
+                {children}
+            </UserContext.Provider>
+        );
+    }
 };
 
 export const useUser = () => {
